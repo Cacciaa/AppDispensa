9a8198548b18f056b5eff4104a76d698af5e4da8
Loryerba <72017837+Loryerba@users.noreply.github.com>
2024-04-05 07:03:10 +0200

    DB Connection stable 1.0

--- AppWithDB/app/src/main/java/com/example/appwithdb/AppDatabase.kt
+++ AppWithDB/app/src/main/java/com/example/appwithdb/AppDatabase.kt
@@ -9,6 +9,25 @@ import androidx.room.RoomDatabase
 abstract class AppDatabase : RoomDatabase() {
     abstract fun userDao(): UserDao
 
-    private fun Builddatabase(context: Context)= Room.databaseBuilder(context.applicationContext,AppDatabase::class.java,"Taskbardb").build()
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase{
+            val tempInstance = INSTANCE
+            if(tempInstance != null){
+                return tempInstance
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "dbprova"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 
 }
--- AppWithDB/app/src/main/java/com/example/appwithdb/MainActivity.kt
+++ AppWithDB/app/src/main/java/com/example/appwithdb/MainActivity.kt
@@ -6,31 +6,36 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.TextView
-import androidx.lifecycle.lifecycleScope
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.room.Room
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     var db: AppDatabase? = null
+    private lateinit var mUserViewModel: UserViewModel
         @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        lifecycleScope.launch {
-           var db = Room.databaseBuilder(
-                applicationContext,
-                AppDatabase::class.java, "dbprova"
-            ).allowMainThreadQueries().build()
+            mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+            mUserViewModel.addUser(User(1,"Lorenzo","Erba"))
+            mUserViewModel.addUser(User(2,"Matteo","Cacciarino"))
+            mUserViewModel.readAllData.observe(this, Observer { user->
+                Toast.makeText(this,user.get(1).firstName,Toast.LENGTH_LONG).show()
 
-            val userDao = db.userDao()
-            //userDao.insertAll(User(3,"Lorenzo","Erba"))
-            val f = findViewById<TextView>(R.id.txtId)
-            f.text = userDao.getAll().get(1).uid.toString()
+            })
+            //mUserViewModel.readAllData.removeObservers(this)
 
-        }
+
     }
 
+
+    fun clicca(view: View?){
+        mUserViewModel.deleteUser(User(2,"Lara","Erba"))
+    }
 
 
 }
